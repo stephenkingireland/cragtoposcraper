f35f4c59f7bb99b18703239c9727e1911837c5eb
Stephen King <stephenkingireland@gmail.com>
2017-12-11 20:54:21 +0000

    Added scraper to project

---
+++ 27crags/Data/javascript/scraper.js
@@ -0,0 +1,27 @@
+﻿var list = [];
+
+var currentSection ="";
+
+$(".climb").each(
+    function(i,val)
+    {
+        var climb = {};
+
+        var section = $(val).prevAll(".buttress").first().text().trim();
+
+        var grade = $(val).find("[nowrap*='nowrap']").text();
+
+        var name = $(val).find(".name > a").text();
+        
+        
+        climb.grade = grade.trim();
+        climb.name = name;
+        climb.section = !section ? currentSection : section;
+
+        currentSection = climb.section;
+
+        list.push(climb);
+    }
+)
+
+JSON.stringify(list);
